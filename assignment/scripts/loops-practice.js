784b6091b52d1ae721d2788d5b1a13d356cc24cf
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-04-14 16:50:17 -0500

    While loop3.a. Answered

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -49,8 +49,23 @@ console.log(star);
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
-
+let index = 0
+while(index < 5){
+  console.log(stars[index]);
+  index++;
+} 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
-
+/*console.log('count from 0 to 5');
+index = 0;
+const max = 10;
+while(index < 6){
+  console.log('In while loop index is:',index);
+  index++;
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+index = 10
+while (index > 4){
+  console.log('In while loop index is:',index);
+  index--;
+}*/
